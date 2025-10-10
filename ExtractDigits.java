960b74a937ddd584aaaa8dfadc9ad5e0a216ecc6
NathanASC25 <javanathanjchin@gmail.com>
2025-10-10 16:09:35 -0400

    first commit

---
+++ ExtractDigits.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class ExtractDigits {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+	System.out.print("\nEnter an integer: ");
+	int n = sc.nextInt();
+	int multiplier = 1;
+	int count = 1;
+	boolean wasNegative = n < 0;
+	if (n < 0) {
+	    n *= -1;
+	}
+	while (n >= multiplier * 10) {
+	    multiplier *= 10;
+	}
+        while (multiplier >= 1) {
+	    int tempInput = n;
+	    if (wasNegative && count == 1) { 
+		System.out.print("-");
+	    }
+	    while (tempInput > 10) {
+		if (count > 1 && multiplier > 1) {
+		    int tempMulti = multiplier;
+		    tempInput /= multiplier;
+		    if (multiplier >= tempInput) {
+			do {
+			    tempMulti /= 10;
+			} 
+			while (tempMulti > 10);
+			tempInput %= tempMulti;
+		    }
+		    else {
+			if (count > 2 && tempInput % multiplier >= 10) {
+			    do {
+				tempMulti /= 10;
+			    }
+			    while (tempMulti > 10);
+			}
+			tempInput %= tempMulti;
+		    }
+		}
+		else if (multiplier == 1) {
+		    tempInput %= (multiplier * 10);
+		}
+		else {
+		    tempInput /= multiplier;
+		}
+	    }
+	    count++;
+	    System.out.printf("%d ", tempInput);
+	    multiplier /= 10;
+	}
+	System.out.print("\n");
+    }
+}
