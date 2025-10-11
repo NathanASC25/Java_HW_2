9489ef7ac8ebc9d06934672c212def515c12858c
NathanASC25 <javanathanjchin@gmail.com>
2025-10-11 02:42:29 -0400

    fourth commit

--- JavaWorkbook.java
+++ JavaWorkbook.java
@@ -1,24 +1,8 @@
 public class JavaWorkbook {
     public static void main(String[] args){
-	int a, b, c, d, e;
-
-	a = 1;
-	b = 18;
-	c = 6;
-	d = 42;
-	e = 8;
-	if (!(a == b && c * c <= 100 || d != e/8)) {
-	    System.out.println("valid");
+    	char count;
+	for (count = 'a'; count <= 'h'; count++) {
+	    System.out.println("HELLO");
 	}
-	else {
-            System.out.println("invalid");
-	}
-	if (1000 > a + b * b * 10 || !(e * d == a / d)){
-	    a = a + b;
-	}
-	else {
-	    a = a - b;
-	}
-	System.out.println(a);
     }
 }
